7cad2005730f2a088edfc85e9ec1efd0161a8ce7
BOUGUET Theo <theo.bougue@gmail.com>
2022-11-10 17:52:20 -0500

    test anim rectangle

--- Projet_Pigeon/src/application/Main.java
+++ Projet_Pigeon/src/application/Main.java
@@ -2,16 +2,38 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 
+import java.io.FileInputStream;
+
+import javafx.animation.*;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Group root = new Group();
 			Scene scene = new Scene(root,400,400);
+			
+			Rectangle rect = new Rectangle(0,0,100,100);
+			rect.setFill(Color.BLUE);
+			
+			
+			
+			TranslateTransition tt = new TranslateTransition(Duration.millis(2000), rect);
+			tt.setByX(200);
+			tt.setByY(200);
+			
+			tt.play();
+			
+			root.getChildren().add(rect);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- Projet_Pigeon/src/module-info.java
+++ Projet_Pigeon/src/module-info.java
@@ -1,5 +1,7 @@
 module Projet_Pigeon {
 	requires javafx.controls;
+	requires javafx.graphics;
+
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
